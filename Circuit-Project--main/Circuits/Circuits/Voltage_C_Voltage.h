0d06a10aae4a4d5890e62fc2e8d368d1fdab8d98
Ahmed-H <76230133+Ahmed-Hany-F@users.noreply.github.com>
2021-01-12 21:49:56 +0200

    A

--- Circuit-Project--main/Circuits/Circuits/Voltage_C_Voltage.h
+++ Circuit-Project--main/Circuits/Circuits/Voltage_C_Voltage.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Dependent_sources.h"
-class Voltage_C_Voltage: Dependent_sources
+class Voltage_C_Voltage :public Dependent_sources
 {
 public:
 	Voltage_C_Voltage(int x1, int x2, string n, int d1, int d2, double coff);
